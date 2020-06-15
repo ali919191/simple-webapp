c54a462bc56519748d106556dd2b28d62f0a9db5
ali919191 <64943083+ali919191@users.noreply.github.com>
2020-06-15 16:37:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def main():
 def hello():
     return 'I am good, how about you?'
 
+@app.route('/fyf')
+def hello():
+    return 'thank you, same to you :)'
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
